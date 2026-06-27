8c3f61f6b57c1eebaec7631a6aa5b1d82cb49030
FiorentinoAgnese298968 <s298968@studenti.polito.it>
2026-06-27 18:52:02 +0200

    Punto 2

--- UI/controller.py
+++ UI/controller.py
@@ -26,9 +26,11 @@ class Controller:
 
     def fillDDAlbum(self):
         self._view._dd_album.options.clear()
+        self._view._dd_album2.options.clear()
         album = self._model.getAllNodes()
         for a in album:
             self._view._dd_album.options.append(ft.dropdown.Option(key=a.AlbumId, text=a.Title))
+            self._view._dd_album2.options.append(ft.dropdown.Option(key=a.AlbumId, text=a.Title))
         self._view.update_page()
 
     def handleBilancio(self, e):
@@ -46,5 +48,30 @@ class Controller:
             self._view.txt_result.controls.append(ft.Text(f"{v[0]} -> Bilancio: {v[1]}"))
         self._view.update_page()
 
-    def handlePercorso(self):
-        pass
+    def handlePercorso(self, e):
+        self._view.txt_result.controls.clear()
+        id1 = self._view._dd_album.value
+        id2 = self._view._dd_album2.value
+        if id1 is None or id2 is None:
+            self._view.create_alert("Seleziona entrambi gli album")
+            return
+        if id1 == id2:
+            self._view.create_alert("L'album di partenza e di arrivo devono essere diversi")
+            return
+        soglia = self._view._txt_soglia.value
+        if soglia == "":
+            self._view.create_alert("Seleziona soglia")
+            return
+        try:
+            x = float(soglia)
+        except ValueError:
+            self._view.create_alert("Seleziona soglia numerica")
+            return
+        path = self._model.getPath(id1, id2, x)
+        if path is None or len(path) <= 1:
+            self._view.txt_result.controls.append(
+                ft.Text(f"Non esiste nessun cammino tra i due album che rispetti i criteri indicati.")
+            )
+        for a in path:
+            self._view.txt_result.controls.append(ft.Text(f"{a}"))
+        self._view.update_page()
--- model/model.py
+++ model/model.py
@@ -7,6 +7,54 @@ class Model:
     def __init__(self):
         self._graph= nx.DiGraph()
         self._idMapA= {}
+        self._path = []
+
+    #def getPath(...):
+
+        # inizializza parziale
+        # imposta record migliore
+        # chiama _ricorsione
+        # return record migliore
+
+    #def _ricorsione(...):
+
+        # 1. CONTROLLO TERMINAZIONE
+        # se ultimo nodo == destinazione:
+        # aggiorna record migliore se necessario
+
+        # 2. ESPLORAZIONE VICINI
+        # per ogni vicino:
+        # se (condizione 1) e (condizione 2):
+        # append()
+        # ricorsione()
+        # pop()
+
+    def getPath(self, id1, id2, soglia):
+        album1 = self._idMapA[int(id1)]
+        album2 = self._idMapA[int(id2)]
+        parziale = [album1]
+        bilancio = self.calcolaBilancio(album1)
+        self._ricorsione(parziale, album2, bilancio, soglia)
+        return self._path
+
+    def _ricorsione(self, parziale, target, bilancio, soglia):
+        if parziale[-1] == target:
+            if len(parziale) > len(self._path):
+                self._path = list(parziale)
+        nodo_corrente = parziale[-1]
+        for vicino in self._graph.neighbors(nodo_corrente):
+            if vicino not in parziale:
+                if self._graph[nodo_corrente][vicino]['weight'] >= soglia:
+                    if self.calcolaBilancio(vicino) > bilancio:
+                        parziale.append(vicino)
+                        self._ricorsione(parziale, target, bilancio, soglia)
+                        parziale.pop()
+
+    def calcolaBilancio(self, source):
+        in_d = self._graph.in_degree(source, weight='weight')
+        out_d = self._graph.out_degree(source, weight='weight')
+        bilancio = in_d - out_d
+        return bilancio
 
 
     def buildGraph(self, n):
